821303b2a83506f0ec56bb13149b47384d6d8200
mikebars1995 <85949125+mikebars1995@users.noreply.github.com>
2021-06-15 17:57:44 +0300

    second commit

--- alert.js
+++ alert.js
@@ -1,8 +1,8 @@
 // {
-//     let isGreater = 5 > 20;
 //     let message = 'Hello!';
 //     alert(message);
 //     alert(`Внешний скрипт ${message}`);
 //     alert(isGreater);
-//     alert(typeof isGreater);
-// }
+
+
+let res
