ddc5b9f1bb76286d52dab343334e6abb5b525c47
mikebars1995 <85949125+mikebars1995@users.noreply.github.com>
2021-06-23 21:42:54 +0300

    prototype

---
+++ prototype.js
@@ -0,0 +1,34 @@
+let head = {
+    glasses: 1
+};
+
+let table = {
+    pen: 3,
+    __proto__: head
+};
+
+let bed = {
+    sheet: 1,
+    pillow: 2,
+    __proto__: table
+};
+
+let pockets = {
+    money: 2000,
+    __proto__: bed
+};
+
+console.log(bed.glasses)
+console.log(pockets.pen)
+
+
+Function.prototype.defer = function (ms) {
+    let f = this
+    return function (...args) {
+        setTimeout(() => f.apply(this, args), ms)
+    }
+}
+function f(a, b) {
+    console.log(a + b)
+}
+f.defer(1000)(3, 7)
