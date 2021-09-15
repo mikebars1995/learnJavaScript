561ea35de5b2824bb761afbd81e82a025d4c1095
mikebars1995 <85949125+mikebars1995@users.noreply.github.com>
2021-09-15 19:10:05 +0300

    moveAndEdit

--- ball/move.js
+++ ball/move.js
@@ -1,6 +1,31 @@
 // field.style.top = 200 + 'px';
 // field.style.left = 400 + 'px';
 // control.style.top = field.clientTop +'px'
+// ball.tabIndex = 1
+// ball.onclick = function() {
+//   this.style.left = this.getBoundingClientRect().left + 'px';
+//   this.style.top = this.getBoundingClientRect().top + 'px';
+
+//   this.style.position = 'fixed';
+// };
+// ball.onkeydown = function(e) {
+//     switch (e.key) {
+//       case 'ArrowLeft':
+//         this.style.left = parseInt(this.style.left) - this.offsetWidth + 'px';
+//         return false;
+//       case 'ArrowUp':
+//         this.style.top = parseInt(this.style.top) - this.offsetHeight + 'px';
+//         return false;
+//       case 'ArrowRight':
+//         this.style.left = parseInt(this.style.left) + this.offsetWidth + 'px';
+//         return false;
+//       case 'ArrowDown':
+//         this.style.top = parseInt(this.style.top) + this.offsetHeight + 'px';
+//         return false;
+//     }
+//   };
+
+
 
 function moveCenter() {
     fieldCoords = field.getBoundingClientRect()
